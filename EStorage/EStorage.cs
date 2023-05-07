dada8c1e3d838254c91cd3152c1b931687c65050
Philip <sphilipkim@gmail.com>
2023-05-07 11:08:19 -0400

    Add project files.

---
+++ EStorage/EStorage.cs
@@ -0,0 +1,15 @@
+namespace EStorage
+{
+    public partial class EStorage : Form
+    {
+        public EStorage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
